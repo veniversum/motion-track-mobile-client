1b8729dbfc73c2516f0af7b6d824954babe15f97
hontat <hontat@gmail.com>
2016-07-30 00:38:39 +0800

    added login logic
    Added validation for 3 login users.

--- app/src/main/java/cxa16/com/shay/LoginActivity.java
+++ app/src/main/java/cxa16/com/shay/LoginActivity.java
@@ -2,16 +2,25 @@ package cxa16.com.shay;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.graphics.Typeface;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -32,6 +41,13 @@ public class LoginActivity extends AppCompatActivity {
 
     Context myContext;
 
+    private int uidFieldLength;
+    private int pwdFieldLength;
+
+    String loginCat;
+
+    private SharedPreferences prefs;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,41 +63,118 @@ public class LoginActivity extends AppCompatActivity {
         fieldUID = (EditText) findViewById(R.id.uid_field);
         fieldPWD = (EditText) findViewById(R.id.uid_field);
 
+        Typeface fontLogo = Typeface.createFromAsset(getAssets(), "FREESCPT.TTF");
         Typeface fontLight = Typeface.createFromAsset(getAssets(), "TheSansExtraLight-Plain.ttf");
         Typeface fontBold = Typeface.createFromAsset(getAssets(), "TheSansSemiBold-Plain.ttf");
 
-        titleApp.setTypeface(fontBold);
-
+        titleApp.setTypeface(fontLogo);
         titleTag.setTypeface(fontLight);
 
         loginButton = (Button) findViewById(R.id.button_login);
         loginButton.setOnClickListener(clickedLogin);
-        //mControlsView = findViewById(R.id.fullscreen_content_controls);
-        //mContentView = findViewById(R.id.fullscreen_content);
 
+        validateLogin();
 
-        // Set up the user interaction to manually show or hide the system UI.
-        /*
-        mContentView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                toggle();
+        fieldUID.addTextChangedListener(new TextWatcher(){
+            public void afterTextChanged(Editable s) {
+                uidFieldLength = s.length();
+                validateLogin();
             }
+            public void beforeTextChanged(CharSequence s, int start, int count, int after){}
+            public void onTextChanged(CharSequence s, int start, int before, int count){}
         });
-*/
-        // Upon interacting with UI controls, delay any scheduled hide()
-        // operations to prevent the jarring behavior of controls going away
-        // while interacting with the UI.
-        //findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
+
+
+        fieldPWD.addTextChangedListener(new TextWatcher(){
+            public void afterTextChanged(Editable s) {
+                pwdFieldLength = s.length();
+                validateLogin();
+            }
+            public void beforeTextChanged(CharSequence s, int start, int count, int after){}
+            public void onTextChanged(CharSequence s, int start, int before, int count){}
+
+        });
+
+        prefs = this.getSharedPreferences(
+                "com.example.app", Context.MODE_PRIVATE);
+
+    }
+
+
+
+
+    private void validateLogin () {
+
+        if (uidFieldLength > 2 && pwdFieldLength > 2){
+            loginButton.setEnabled(true);
+            loginButton.setBackgroundColor(Color.argb(255, 63, 100, 168 ));
+        } else {
+            loginButton.setEnabled(false);
+            loginButton.setBackgroundColor(Color.GRAY);
+        }
+
+    }
+
+    private void submitLogin () {
+
+        String uidInput = fieldUID.getText().toString();
+        Boolean valid = false;
+
+        if (uidInput.equals("renjie")){
+            valid = true;
+            loginCat = "Normal";
+        } else if (uidInput.equals("qingzhuo")){
+            valid = true;
+            loginCat = "Stationary";
+        } else if (uidInput.equals("malie")){
+            valid = true;
+            loginCat = "Food";
+        }
+
+        if (valid){
+
+            prefs.edit().putString("category", loginCat).apply();
+            prefs.edit().putString("loggedInId", uidInput).apply();
+
+            Intent intent = new Intent(myContext, WalletActivity.class);
+            startActivity(intent);
+        } else {
+            erroLogin();
+        }
+    }
+
+    private void erroLogin () {
+
+        AlertDialog alertDialog = new AlertDialog.Builder(LoginActivity.this).create();
+        alertDialog.setTitle("Login Error");
+        alertDialog.setMessage("Invalid Login Id");
+        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        alertDialog.show();
+
     }
 
     View.OnClickListener clickedLogin = new View.OnClickListener() {
         public void onClick(View v) {
-            // it was the 1st button
-            Intent intent = new Intent(myContext, WalletActivity.class);
-            startActivity(intent);
+            submitLogin();
         }
     };
+
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        View view = getCurrentFocus();
+        if (view != null) {
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.
+                    INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+        }
+        return true;
+    }
 
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
