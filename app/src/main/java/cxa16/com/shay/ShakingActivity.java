a175ca3a680bc7cdd39ac967a5abdb12893baf43
RenJieChong <bl2ead@gmail.com>
2016-07-30 03:52:49 +0800

    Changes to startSHaking and on-going shaking activities

--- app/src/main/java/cxa16/com/shay/ShakingActivity.java
+++ app/src/main/java/cxa16/com/shay/ShakingActivity.java
@@ -3,13 +3,20 @@ package cxa16.com.shay;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.CountDownTimer;
+import android.os.PersistableBundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
-
+import android.view.animation.Animation;
+import android.view.animation.ScaleAnimation;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 
 /**
@@ -19,42 +26,55 @@ import android.view.View;
 public class ShakingActivity extends AppCompatActivity {
 
     Context myContext;
+    private TextView mTitle;
+    private TextView mInstructions;
+    private ProgressBar mProgressBar;
+    private CountDownTimer mCountDownTimer;
+    private int i;
 
-
-    final Handler handler = new Handler();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
         myContext = this;
-
         setContentView(R.layout.activity_shaking);
+        mTitle = (TextView) findViewById(R.id.title_wallet);
+        mInstructions = (TextView) findViewById(R.id.shaking_instruction);
 
+        i = 0;
+        mProgressBar = (ProgressBar) findViewById(R.id.progress_bar);
+        mProgressBar.setProgress(i);
+        mCountDownTimer=new CountDownTimer(3000,29) {
 
-        handler.postDelayed(new Runnable() {
             @Override
-            public void run() {
-
-                paymentDone();
+            public void onTick(long millisUntilFinished) {
+                i++;
+                mProgressBar.setProgress(i);
 
             }
-        }, 3000);
-    }
 
-    private void paymentDone () {
-
-        /*
-        Intent intent = new Intent(myContext, PaymentDoneActivity.class);
-        startActivity(intent);
-*/
+            @Override
+            public void onFinish() {
+                //Do what you want
+                i++;
+                mProgressBar.setProgress(i);
+                mTitle.setText("Awaiting Server Response");
+            }
+        };
+        mCountDownTimer.start();
     }
 
     @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
+    protected void onResume() {
+        super.onResume();
 
     }
 
+    @Override
+    public void onPostCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onPostCreate(savedInstanceState, persistentState);
+        Typeface fontLight = Typeface.createFromAsset(getAssets(), "TheSansExtraLight-Plain.ttf");
+        mTitle.setTypeface(fontLight);
+        mInstructions.setTypeface(fontLight);
+    }
 
 }
