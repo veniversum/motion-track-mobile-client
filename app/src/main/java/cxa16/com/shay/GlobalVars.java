10b95a78d238cfa962ffe1675768c0d207ba0cbe
hontat <hontat@gmail.com>
2016-07-30 07:00:43 +0800

    added transaction to done page on success.
    added transaction to done page on success.

--- app/src/main/java/cxa16/com/shay/GlobalVars.java
+++ app/src/main/java/cxa16/com/shay/GlobalVars.java
@@ -8,7 +8,9 @@ import android.app.Application;
 public class GlobalVars extends Application {
     private boolean isSender;
     private float amount;
+    private float total;
     private String user;
+    private String other;
 
     public float getAmount() {
         return amount;
@@ -18,12 +20,27 @@ public class GlobalVars extends Application {
         this.amount = amount;
     }
 
+    public float getTotal() {
+        return total;
+    }
+
+    public void setTotal(float total) {
+        this.total = total;
+    }
+
     public String getUser() {
         return user;
     }
 
     public void setUser(String user) {
         this.user = user;
+    }
+
+
+    public String getOther() {return other;}
+
+    public void setOther(String other) {
+        this.other = other;
     }
 
     public boolean getSender() {
