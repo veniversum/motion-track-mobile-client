f5e1209b1485a26f1eb9d213d4b649b8f12f16e8
hontat <hontat@gmail.com>
2016-07-30 04:13:33 +0800

    edit payment done page
    edit payment done page

--- app/src/main/java/cxa16/com/shay/PaymentDoneActivity.java
+++ app/src/main/java/cxa16/com/shay/PaymentDoneActivity.java
@@ -1,18 +1,26 @@
 package cxa16.com.shay;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.ScaleAnimation;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
 public class PaymentDoneActivity extends AppCompatActivity {
+
+    private ImageView icon_success;
 
 
     @Override
@@ -21,6 +29,30 @@ public class PaymentDoneActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_payment_done);
 
+
+        icon_success = (ImageView) findViewById(R.id.icon_success);
+        icon_success.setVisibility(View.GONE);
+
+
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        icon_success.setVisibility(View.VISIBLE);
+        scaleView(icon_success, 0, 1);
+    }
+
+
+    public void scaleView(View v, float startScale, float endScale) {
+        Animation anim = new ScaleAnimation(
+                1f, 1f, // Start and end values for the X axis scaling
+                startScale, endScale, // Start and end values for the Y axis scaling
+                Animation.RELATIVE_TO_SELF, 0f, // Pivot point of X scaling
+                Animation.RELATIVE_TO_SELF, 1f); // Pivot point of Y scaling
+        anim.setFillAfter(true); // Needed to keep the result of the animation
+        v.startAnimation(anim);
     }
 
 
